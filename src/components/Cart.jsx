bcf4f1e5d1c5adc24156d0bec484ab5b3d4cd14a
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-06-26 21:24:24 +0530

    added cart item component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,29 +3,36 @@ import Button from "./UI/Button";
 import { useContext } from "react";
 import { CartContext } from "../store/CartContext";
 import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
 
 export default function Cart() {
   const ctxValue = useContext(CartContext);
 
-  const {progress, hideCart} = useContext(UserProgressContext);
+  const { progress, hideCart } = useContext(UserProgressContext);
 
   const totalCartPrice = ctxValue.items.reduce(
     (totalPrice, item) => totalPrice + item.price * item.quantity,
     0
   );
+
   return (
-    <Modal className="cart" open={progress === 'cart'}>
+    <Modal className="cart" open={progress === "cart"}>
       <h2>Your Cart</h2>
       <ul>
         {ctxValue.items.map((item) => (
-          <li key={item.id}>
-            {item.price} - {item.quantity}
-          </li>
+          <CartItem
+            key={item.id}
+            {...item}
+            onIncrease={() => ctxValue.addItem(item)}
+            onDecrease={() => ctxValue.removeItem(item.id)}
+          ></CartItem>
         ))}
       </ul>
       <p className="cart-total">Total Price: {totalCartPrice}</p>
       <p className="modal-actions">
-        <Button textOnly onClick={hideCart}>Close</Button>
+        <Button textOnly onClick={hideCart}>
+          Close
+        </Button>
         <Button>Checkout</Button>
       </p>
     </Modal>
