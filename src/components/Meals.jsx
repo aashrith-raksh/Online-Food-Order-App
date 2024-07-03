a7c7030a54ba4eb3507e7b90a1bafc891038c48b
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-07-03 20:25:29 +0530

    changed Meals to use useFetch

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,33 +1,22 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import MealItem from "./MealItem";
+import useFetch from "./hooks/useFetch";
 
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      // setIsLoading(true);
-      const response = await fetch("http://localhost:3000/meals");
+  const {data: loadedMeals, isLoading, error} = useFetch("http://localhost:3000/meals", []);
 
-      if (!response.ok) {
-        // ...
-      }
-      const meals = await response.json();
-      setLoadedMeals(meals);
-      // setIsLoading(false);
-    }
-
-    fetchMeals();
-  }, []);
+  if(isLoading){
+    return <p className="center">Fetching Meals...</p>
+  }
+  if(error){
+    return <p className="center">{error}</p>
+  }
 
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => {
-        {
-          /* console.log("rendering meal:", meal); */
-        }
         return <MealItem key={meal.id} meal={meal} />;
       })}
     </ul>
